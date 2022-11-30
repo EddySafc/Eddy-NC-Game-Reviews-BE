449df1aa50687b88117ed8a53302016a3c23bdf5
EddySafc <eddy@eddy-730QED>
2022-11-30 14:17:07 +0000

    further host setup

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
+
 const config =
   ENV === "production"
     ? {
@@ -14,8 +12,10 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+// if (!process.env.PGDATABASE) {
+//   throw new Error("PGDATABASE not set");
+// }
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
-
 module.exports = new Pool(config);
